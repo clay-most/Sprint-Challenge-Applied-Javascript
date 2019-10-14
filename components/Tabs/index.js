45e40bf054e5bec247a99f053f3961adf594ecb0
Anna Morris <annapatton444@gmail.com>
2019-10-13 19:14:07 -0500

    build tab components

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    const topicsArr = response.data.topics;
+    const topics = document.querySelector(".topics");
+    topicsArr.forEach(topic => {
+      topics.appendChild(TabCreator(topic));
+    });
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
+
+function TabCreator(topic) {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = topic;
+
+  return tab;
+}
